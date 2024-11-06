f7c2c3aa89fb4a1d0eaf36537af18896e6a5c651
Vitalii Oliinyk <oleynik.vitaliy.uah@gmail.com>
2024-11-06 13:41:58 +0200

    Rewrite code to use ALB instead public network

--- main.tf
+++ main.tf
@@ -75,7 +75,32 @@ resource "aws_route_table" "abz_homework_public_rt" {
   }
 }
 
-# Route Table Association
+# NAT Gateway for private subnet Internet access
+resource "aws_eip" "abz_homework_nat_eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "abz_homework_nat_gw" {
+  allocation_id = aws_eip.abz_homework_nat_eip.id
+  subnet_id     = aws_subnet.abz_homework_public_subnet_1.id
+  tags = {
+    Name = "abz-homework-nat-gw"
+  }
+}
+
+# Private Route Table for private subnets with NAT Gateway
+resource "aws_route_table" "abz_homework_private_rt" {
+  vpc_id = aws_vpc.abz_homework_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.abz_homework_nat_gw.id
+  }
+  tags = {
+    Name = "abz-homework-private-rt"
+  }
+}
+
+# Route Table Associations
 resource "aws_route_table_association" "public_subnet_1_rt_association" {
   subnet_id      = aws_subnet.abz_homework_public_subnet_1.id
   route_table_id = aws_route_table.abz_homework_public_rt.id
@@ -84,6 +109,83 @@ resource "aws_route_table_association" "public_subnet_1_rt_association" {
 resource "aws_route_table_association" "public_subnet_2_rt_association" {
   subnet_id      = aws_subnet.abz_homework_public_subnet_2.id
   route_table_id = aws_route_table.abz_homework_public_rt.id
+}
+
+resource "aws_route_table_association" "private_subnet_1_rt_association" {
+  subnet_id      = aws_subnet.abz_homework_private_subnet_1.id
+  route_table_id = aws_route_table.abz_homework_private_rt.id
+}
+
+resource "aws_route_table_association" "private_subnet_2_rt_association" {
+  subnet_id      = aws_subnet.abz_homework_private_subnet_2.id
+  route_table_id = aws_route_table.abz_homework_private_rt.id
+}
+
+# Application Load Balancer
+resource "aws_lb" "abz_homework_alb" {
+  name               = "abz-homework-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.abz_homework_alb_sg.id]
+  subnets            = [
+    aws_subnet.abz_homework_public_subnet_1.id,
+    aws_subnet.abz_homework_public_subnet_2.id
+  ]
+  tags = {
+    Name = "abz-homework-alb"
+  }
+}
+
+# Security Group for ALB
+resource "aws_security_group" "abz_homework_alb_sg" {
+  vpc_id = aws_vpc.abz_homework_vpc.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "abz-homework-alb-sg"
+  }
+}
+
+# Target Group for ALB
+resource "aws_lb_target_group" "abz_homework_tg" {
+  name     = "abz-homework-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.abz_homework_vpc.id
+  health_check {
+    path = "/"
+    port = "traffic-port"
+  }
+  tags = {
+    Name = "abz-homework-tg"
+  }
+}
+
+# ALB Listener
+resource "aws_lb_listener" "abz_homework_listener" {
+  load_balancer_arn = aws_lb.abz_homework_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.abz_homework_tg.arn
+  }
 }
 
 # Security Group for EC2
@@ -236,15 +338,21 @@ resource "aws_iam_instance_profile" "abz_homework_ssm_instance_profile" {
 resource "aws_instance" "abz_homework_ec2" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.abz_homework_public_subnet_1.id
+  subnet_id              = aws_subnet.abz_homework_private_subnet_1.id
   vpc_security_group_ids = [aws_security_group.abz_homework_ec2_sg.id]
-  associate_public_ip_address = true
   key_name               = aws_key_pair.abz_homework_keypair.key_name
   iam_instance_profile   = aws_iam_instance_profile.abz_homework_ssm_instance_profile.name
 
   tags = {
     Name = "abz-homework-ec2"
   }
+}
+
+# Register EC2 Instance with Target Group
+resource "aws_lb_target_group_attachment" "abz_homework_ec2_tg_attachment" {
+  target_group_arn = aws_lb_target_group.abz_homework_tg.arn
+  target_id        = aws_instance.abz_homework_ec2.id
+  port             = 80
 }
 
 # SSM Document for WordPress installation
